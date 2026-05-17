63563257d3a705f918ea4ea4b807895ad6088e21
Panda <suppandochka@gmail.com>
2026-05-18 01:53:21 +0500

    Add files via upload

---
+++ Commannds/engLearn/engLearn.py
@@ -0,0 +1,19 @@
+import pytesseract
+
+# Для считывания PDF
+import PyPDF2
+# Для анализа структуры PDF и извлечения текста
+from pdfminer.high_level import extract_pages, extract_text
+from pdfminer.layout import LTTextContainer, LTChar, LTRect, LTFigure
+# Для извлечения текста из таблиц в PDF
+import pdfplumber
+# Для извлечения изображений из PDF
+from PIL import Image
+from pdf2image import convert_from_path
+# Для выполнения OCR, чтобы извлекать тексты из изображений 
+import pytesseract 
+# Для удаления дополнительно созданных файлов
+import os
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
